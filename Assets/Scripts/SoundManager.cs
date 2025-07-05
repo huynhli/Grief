3b19cdd289a5637207094e8aec9b471e09d5f961
TrachScor6 <liam.huynh@mail.utoronto.ca>
2025-07-04 23:06:48 -0400

    main menu animations + sfx done, need music and player/final animation

---
+++ Assets/Scripts/SoundManager.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    public static SoundManager instance;
+    [SerializeField] private AudioSource soundFXObject;
+
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+    }
+
+    public void PlaySFXClip(AudioClip audioClip, Transform spawnTransform, float volume)
+    {
+        AudioSource audioSource = Instantiate(soundFXObject, spawnTransform.position, Quaternion.identity);
+        audioSource.clip = audioClip;
+        audioSource.volume = volume;
+        audioSource.Play();
+
+        Destroy(audioSource.gameObject, audioClip.length);
+    }
+}
